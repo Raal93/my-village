39685746582cc7e0a460309e7e21d5bdb7cb881e
Rafał Czajkowski <raal93@gmail.com>
2024-02-01 21:29:35 +0100

    gh pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import PageWrapper from './components/organisms/PageWrapper/PageWrapper';
 import ErrorPage from './pages/ErrorPage/ErrorPage';
@@ -11,19 +10,17 @@ import VillagePage from './pages/VillagePage/VillagePage';
 
 const App = () => {
   return (
-    <BrowserRouter basename="/my-village">
-      <Router>
-        <PageWrapper>
-          <Routes>
-            <Route index path="/my-village/" element={<VillagePage />} />
-            <Route index path="/my-village/ratusz" element={<RatuszPage />} />
-            <Route index path="/my-village/settings" element={<SettingsPage />} />
-            <Route index path="/my-village/simulator" element={<SimulatorPage />} />
-            <Route index path="/*" element={<ErrorPage />} />
-          </Routes>
-        </PageWrapper>
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <PageWrapper>
+        <Routes>
+          <Route index path="/my-village/" element={<VillagePage />} />
+          <Route index path="/my-village/ratusz" element={<RatuszPage />} />
+          <Route index path="/my-village/settings" element={<SettingsPage />} />
+          <Route index path="/my-village/simulator" element={<SimulatorPage />} />
+          <Route index path="/*" element={<ErrorPage />} />
+        </Routes>
+      </PageWrapper>
+    </Router>
   );
 };
 
